c88975956c97bebba627790b59c1731e9b62c5a4
KiARC <katiearose@protonmail.com>
2022-02-15 19:02:46 -0500

    Added a new test
    It just generates a bunch of values with each method, there used to be random exceptions with nextDouble, and then they stopped with no apparent changes, so now I'm paranoid, but everything seems fine.

--- lib/src/main/java/com/katiearose/Tester.java
+++ lib/src/main/java/com/katiearose/Tester.java
@@ -4,11 +4,28 @@ import java.util.Arrays;
 
 public class Tester { // Kinda not a great test but better than nothing
   public static void main(String[] args) {
+    distributionTest();
+    stressTest();
+  }
+  public static void distributionTest() {
     CollatzRand c = new CollatzRand();
     long[] j = new long[10];
     for (int i = 0; i < 1000000; i++) {
       j[c.nextInt(10)] += 1;
     }
-    System.out.println(Arrays.toString(j));
+    System.out.println("Distribution: " + Arrays.toString(j));
+  }
+  public static void stressTest() {
+    CollatzRand c = new CollatzRand();
+    for (int i = 0; i < 100000; i++) {
+      c.nextInt();
+      c.nextBoolean();
+      c.nextByte();
+      c.nextDouble();
+      c.nextFloat();
+      c.nextLong();
+      c.nextShort();
+    }
+    System.out.println("No crashes, nice.");
   }
 }
